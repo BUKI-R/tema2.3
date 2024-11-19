8bad1522b592ea31b736398c27bd89b9d70c8527
BUKI <buyunov2005@gmail.com>
2024-11-19 15:29:40 +0900

    Initial commit

---
+++ Class/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class user12
+{
+    public string login{ get; set; }
+    public string Name { get; set; }
+    public string Fam { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string adres { get; set; }
+}
---
+++ Metod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+void Hel(int a, int b)
+{
+    int result = a + b;
+    Console.WriteLine($"{a}+{b}={result}");
+}
+Console.WriteLine($"Введите 2 значения a и b");
+Console.Write("a= ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("b= ");
+int y = Convert.ToInt32(Console.ReadLine());
+Hel(x, y);
+private void people(string name, int age)
+{
+    Console.WriteLine($"Имя пользователя:{name};");
+    Console.WriteLine($"Возраст пользователя:{age};");
+}
+string na = Console.ReadLine();
+int ag = Convert.ToInt32(Console.ReadLine());
+people(na,ag);
